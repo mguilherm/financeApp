52a98798973a79de6b97bea7518fba7106338b87
Marcos Guilherme Silva <marcosguilherme.silva@outlook.com>
2023-07-25 17:34:54 -0300

    initial

---
+++ src/components/Balance/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Balance() {
+ return (
+   <View>
+    <Text style={styles.container}>Teste</Text>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    width: '100%',
+  }
+})
+
